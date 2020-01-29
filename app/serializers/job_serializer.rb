1be8e69a06e1eeb6fd543443fcc3887ba4d79b5d
joffitzer <joffitzer@gmail.com>
2020-01-29 12:09:13 -0500

    Working relationships between jobs and employers

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,6 +1,7 @@
 class JobSerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :location, :category, :summary, :description, :sal_range_low, :sal_range_high, :employer
+  # belongs_to :employer
 end
 
 # t.integer :employer_id
