9006b00167f7e1bc51fa9ba0e229bb617a997943
joffitzer <joffitzer@gmail.com>
2020-01-31 12:00:15 -0500

    added create to user apps controller

--- app/controllers/api/v1/user_apps_controller.rb
+++ app/controllers/api/v1/user_apps_controller.rb
@@ -5,4 +5,15 @@ class Api::V1::UserAppsController < ApplicationController
         render json: UserAppSerializer.new(userApps)
     end 
 
+    def create 
+        userApp = UserApp.create(user_app_params)
+        render json: userApp, except: [:updated_at]
+    end 
+
+end
+
+private
+
+def user_app_params
+    params.require(:user_app).permit(:mini_cl, :job_id, :user_id)
 end
