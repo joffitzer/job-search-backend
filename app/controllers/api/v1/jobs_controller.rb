a842f75b5f955d2feccd093a2813fc82bd7ded8c
joffitzer <joffitzer@gmail.com>
2020-02-07 16:43:43 -0500

    ability to post a new job on employer side and apply to that job from user side

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -5,4 +5,14 @@ class Api::V1::JobsController < ApplicationController
         render json: JobSerializer.new(jobs)
     end 
 
+    def create 
+        job = Job.create(job_params)
+        render json: job, except: [:created_at, :updated_at]
+    end 
+
+    private
+    def job_params
+        params.require(:job).permit(:employer_id, :title, :location, :category, :summary, :description, :sal_range_low, :sal_range_high, :active)
+    end 
+
 end
